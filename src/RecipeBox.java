5bbdb3819b85bc554c63e13b1880be5d09e235d0
Tevin Davis <tevindavis@icloud.com>
2025-08-05 19:45:12 -0400

    Added three methods. 1. method to print all recipes in a list, 2. method to add recipe to arraylist, and 3. method to print details of the selected recipe.

--- src/RecipeBox.java
+++ src/RecipeBox.java
@@ -20,5 +20,37 @@ public class RecipeBox {
     }
 
 
-    
+    // This method prints the details of a recipe based on its name
+	public void printAllRecipeDetails(String recipeName) {
+		for (Recipe recipe : listOfRecipes) {
+			if (recipe.getRecipeName().equalsIgnoreCase(recipeName)) {
+				recipe.printRecipe();
+				return;
+			}
+		}
+		System.out.println("Recipe not found.");
+	}
+
+    // This method prints the names of all recipes in the list
+	public void printAllRecipeNames() {
+		if (listOfRecipes.isEmpty()) {
+			System.out.println("No recipes available.");
+			return;
+		}
+		for (int i = 0; i < listOfRecipes.size(); i++) {
+			System.out.println((i + 1) + ": " + listOfRecipes.get(i).getRecipeName());
+		}
+	}
+
+    // This method adds a new recipe to the listOfRecipes ArrayList
+	public void addNewRecipe() {
+		Recipe newRecipe = new Recipe();
+		newRecipe.createNewRecipe();
+		if (newRecipe.getRecipeName() != null && !newRecipe.getRecipeName().isEmpty()) {
+			listOfRecipes.add(newRecipe);
+			System.out.println("Recipe added: " + newRecipe.getRecipeName());
+		} else {
+			System.out.println("Invalid recipe.");
+		}
+	}
 }
