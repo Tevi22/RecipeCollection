e12bdfe223a1889577d06e501362a4bb9430b564
Tevin Davis <tevindavis@icloud.com>
2025-08-05 19:07:17 -0400

    added array list variable for list of rcipes, Also added Accessor,Mutator, and default constructor.

--- src/RecipeBox.java
+++ src/RecipeBox.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class RecipeBox {
+
+    private ArrayList<Recipe> listOfRecipes;
+
+    //Accessor method to get the list of recipes
+    public ArrayList<Recipe> getListOfRecipes() {
+		return listOfRecipes;
+	}
+
+    // Mutator method to set the list of recipes
+	public void setListOfRecipes(ArrayList<Recipe> listOfRecipes) {
+		this.listOfRecipes = listOfRecipes;
+	}
+
+    // Constructor to initialize the list of recipes
+    public RecipeBox() {    
+        this.listOfRecipes = new ArrayList<Recipe>();
+    }
+
+
     
 }
