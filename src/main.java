76fc070335aa7b6c988b63c0e57dc8563115a8c9
Tevin Davis <tevindavis@icloud.com>
2025-08-06 14:49:45 -0400

    Added custom method scalefactor choice to menu to scale recipe ingredients

--- src/main.java
+++ src/main.java
@@ -12,7 +12,8 @@ public class Main {
             System.out.println("1. Add a new recipe");
             System.out.println("2. List all recipe names");
             System.out.println("3. View a recipe");
-            System.out.println("4. Quit");
+            System.out.println("4. Scale a recipe"); //Custom method to scale recipe ingredients
+            System.out.println("5. Quit");
             System.out.print("Enter choice: ");
 
             int choice;
@@ -23,9 +24,6 @@ public class Main {
             choice = scnr.nextInt();
             scnr.nextLine(); // consume newline
 
-            //FIXME Add custom method choice to menu to scale recipe ingredients
-
-
             switch (choice) {
                 case 1:
                     // Create new recipe using the createNewRecipe() method from Recipe class
@@ -33,16 +31,56 @@ public class Main {
                     break;
 
                 case 2:
+                    // Print all recipe names using the printAllRecipeNames() method from RecipeBox class
                     myRecipeBox.printAllRecipeNames();
                     break;
 
                 case 3:
+                    // Print details of a specific recipe using the printAllRecipeDetails() method from RecipeBox class
                     System.out.print("Enter the name of the recipe to view: ");
                     String selectedRecipeName = scnr.nextLine();
                     myRecipeBox.printAllRecipeDetails(selectedRecipeName);
                     break;
 
                 case 4:
+                    // Show all recipes so the user can choose
+                    System.out.println("Select a recipe to scale:");
+                    for (int i = 0; i < listOfRecipes.size(); i++) {
+                        System.out.println((i + 1) + ". " + listOfRecipes.get(i).getRecipeName());
+                    }
+
+                    // Get user's choice
+                    int recipeIndex = -1;
+                    while (recipeIndex < 1 || recipeIndex > listOfRecipes.size()) {
+                        System.out.print("Enter recipe number: ");
+                            if (scnr.hasNextInt()) {
+                            recipeIndex = scnr.nextInt();
+                        } else {
+                        System.out.println("Invalid input. Please enter a number.");
+                        scnr.next(); // clear invalid input
+                        }
+                    }
+
+                    // Ask for scale factor
+                    double scaleFactor = 0;
+                    while (scaleFactor <= 0) {
+                        System.out.print("Enter scale factor (e.g., 2.0 for double, 0.5 for half): ");
+                        if (scnr.hasNextDouble()) {
+                            scaleFactor = scnr.nextDouble();
+                            if (scaleFactor <= 0) {
+                                System.out.println("Scale factor must be greater than zero.");
+                            }
+                        } else {
+                            System.out.println("Invalid input. Please enter a number.");
+                            scnr.next(); // clear invalid input
+                        }
+                    }
+
+                    // Scale the selected recipe
+                    listOfRecipes.get(recipeIndex - 1).scaleRecipe(scaleFactor);
+                    break;
+
+                case 5:
                     quit = true;
                     System.out.println("Exiting Recipe Box. Goodbye!");
                     break;
